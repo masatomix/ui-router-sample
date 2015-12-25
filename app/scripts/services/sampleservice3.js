180a74b83ba86909eab8efa61c68fac7cb846dae
masatomix <masatomix@ki-no.org>
2015-12-25 15:02:45 +0900

    sample

---
+++ app/scripts/services/sampleservice3.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name uiRouterSampleApp.SampleService3
+ * @description
+ * # SampleService3
+ * Factory in the uiRouterSampleApp.
+ */
+angular.module('uiRouterSampleApp')
+  .factory('SampleService3', function () {
+    return {
+      someMethod1: function () {
+        var ret = {
+          value1: "1",
+          value2: "2",
+          value3: "3"
+        };
+        return ret;
+      },
+      someMethod2: function () {
+        var ret = {
+          value1: "10",
+          value3: "30"
+        };
+        return ret;
+      }
+    };
+  });
