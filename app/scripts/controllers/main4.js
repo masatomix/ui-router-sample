ff624c9fcf711ceacb7026d9c31226e2e9e6232f
masatomix <masatomix@ki-no.org>
2016-01-26 17:15:37 +0900

    commit

--- app/scripts/controllers/main4.js
+++ app/scripts/controllers/main4.js
@@ -8,13 +8,19 @@
  * Controller of the uiRouterSampleApp
  */
 angular.module('uiRouterSampleApp')
-  .controller('Main4Ctrl', function ($scope) {
+  .controller('Main4Ctrl', ['WeatherService2', '$scope', function (WeatherServices2, $scope) {
 
     $scope.onclick1 = function () {
-      $scope.data = SampleService3.someMethod1();
+      console.log('button1');
+      $scope.data = WeatherService2.getWeather1();
     }
     $scope.onclick2 = function () {
-      $scope.data = SampleService3.someMethod2();
+      console.log('button1');
+      $scope.data = WeatherService2.getWeather2();
+    }
+    $scope.onclick3 = function () {
+      console.log('button3');
+      $scope.data = WeatherService2.getWeather3();
     }
 
-  });
+  }]);
